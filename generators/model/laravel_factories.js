d60b0e397124dd54a8ea91eb8163dc28aa855b19
Tommy <tom.staszyszyn@gmail.com>
2016-01-28 21:24:06 +0000

    clean up

--- generators/model/laravel_factories.js
+++ generators/model/laravel_factories.js
@@ -6,231 +6,170 @@ var exports = module.exports = {},
 
 
 
- var generators = require('yeoman-generator');
+var generators = require('yeoman-generator');
 
-function migration(){
-      this.prepare = function(){
-        this.prepareMigration();
-      }
-}
-
-var laraObjects = {
-    migration: migration
-}
-
- exports.Base = generators.Base.extend({
+exports.Base = generators.Base.extend({
   
-   constructor: function() {
+  constructor: function () {
+    generators.Base.apply(this,arguments);
+    this.argument('name', { type: String, required: true });
+    this.option('fields', {desc: 'fields for model'});
+    this.modelProperties = [];
+  },
+      
+  addFactory: function () { 
+    this.prepareFactory(); 
+  },
+  
+  addMigration: function () { 
+    this.prepareMigration(); 
+  }, 
 
-      generators.Base.apply(this,arguments);
-      this.argument('name', { type: String, required: true });
-      this.option('fields', {desc: 'fields for model'});
-      this.modelProperties = [];
-   },
-   
-   factory: function (context) {
+  prepareMigration: function () {
+    var migration = this.fs.copyTpl(
+      this.templatePath(locs.db.modelMigration),
+      this.destinationPath(this.getMigrationFileName()),
+      {
+        'name': this.name.toLowerCase(),
+        'fields': this.buildMigrationInsert()
+      });
+  },
+  
+  getMigrationFileName: function () {
+    return locs.db.modelMigrationDir + "create_" 
+           + this.name.toLowerCase() + "s_table.php"
+  },
 
-      function prepare(){
-        context.prepareFactory();
+  prepareFactory: function() {
+    var current = wiring.readFileAsString(this.destinationPath(locs.db.modelFactory)),
+        factoryInsert = this.buildFactoryInsert(),
+        newFactory = current + "\n" + factoryInsert;
+
+        wiring.writeFileFromString(
+              newFactory,
+              this.destinationPath(locs.db.modelFactory)
+        );
+  },
+
+  buildMigrationInsert: function()
+  {
+        var fields = [],
+            i = 0;
+        
+        for(i; i < this.modelProperties.length; i++){
+          fields.push(this.formatMigrationField(this.modelProperties[i]));
+        }
+
+        return fields.join("\n");
+    
+  },
+
+  buildFactoryInsert: function()
+  {
+        var pathInsert = this.templatePath(locs.db.modelFactoryInsert),
+        newFactory = wiring.readFileAsString(pathInsert),
+        i = 0,
+        fields = [];
+        
+        for(i; i < this.modelProperties.length; i++){
+          fields.push(this.formatFactoryField(this.modelProperties[i]));
+        }
+
+        return newFactory
+          .replace("<fields>", fields.join(",\n"))
+          .replace("<modelName>",this.name);
+  },
+
+  formatFactoryField: function(modelField)
+  {
+        return '\t"'+ modelField[0] +'" => ' 
+                    + this.getFaker(modelField[1]);
+  },
+
+  formatMigrationField: function(modelField)
+  {
+        return '\t'+ this.getMigration(modelField[1])
+                    + "');"
+  },
+
+  getMigration: function (fieldType)
+  {
+    switch (fieldType) {
+        case 'string':
+          return "$table->string('";
+        case 'double':  
+          return "$table->double('";
+        case 'integer':
+          return "$table->integer('";
+        case 'datetime':
+          return "$table->datetime('";
+        case 'text':
+          return "$table->text('";
+        default:
+          return "$table->string('";
+    }
+  },
+     
+  getFaker: function(fieldType){
+      switch(fieldType){
+        case 'string':
+          return "$faker->name";
+        case 'double':  
+        case 'integer':
+          return "$faker->randomNumber(1)";
+        case 'datetime':
+          return "$faker->datetime()";
+        case 'text':
+          return "$faker->sentence";
+        default:
+          return '$faker->name';
       }
+  },
 
-     return { prepare: prepare }
-   },
-   add: function (what) {
-      var func = this[what](this);
-      this.log(func);
-      func.prepare(); 
-   },
-   
-   addFactory: function() { this.prepareFactory(); },
-   addMigration: function () { this.prepareMigration(); }, 
+  _combine: function(vars, types){
+        var combined = [];
+        vars.forEach(function(element) {
+            combined.push(
+                { 
+                name: element,
+                type: types[element+"_type"]
+                }      
+            );
+        }, this);
+        
+        return combined;
+  },
 
-    prepareMigration: function() {
-      var migration = this.fs.copyTpl(
-          this.templatePath(locs.db.modelMigration),
-          this.destinationPath(locs.db.modelMigrationDir
-            + "create_"
-            + this.name.toLowerCase()
-            + "s_table.php"
-              ),
+  modelQuestions: function() {
+    if(this.options.fields){
+      var i = 0,
+          fields = this.options.fields.split(','),
+          typeQuestions = [],
+          typeAnswers = [],
+          done= this.async();
+          
+          for(i = 0; i < fields.length; i++)
           {
-            'name': this.name.toLowerCase(),
-            'fields': this.buildMigrationInsert()
-          });
-          
-    },
-
-    prepareFactory: function() {
-      var current = wiring.readFileAsString(
-                        this.destinationPath(locs.db.modelFactory)
-                      ),
-          factoryInsert = this.buildFactoryInsert(),
-          newFactory = current + "\n" + factoryInsert;
-
-          wiring.writeFileFromString(
-                newFactory,
-                this.destinationPath(locs.db.modelFactory)
-                );
-
-    },
-
-    buildMigrationInsert: function()
-    {
-          var fields = [],
-              i = 0;
-          
-          for(i; i < this.modelProperties.length; i++){
-            fields.push(this.formatMigrationField(this.modelProperties[i]));
+              typeQuestions.push({
+                  type: 'rawlist',
+                  name: fields[i]+'_type',
+                  message: 'field type for '+fields[i],
+                  choices: [
+                      "string",
+                      "integer",
+                      "datetime",
+                      "text",
+                      "time",
+                      "blob",
+                      "double"
+                  ]});
           }
-
-          return fields.join("\n");
-      
-    },
-
-    buildFactoryInsert: function()
-    {
-          var pathInsert = this.templatePath(locs.db.modelFactoryInsert),
-          newFactory = wiring.readFileAsString(pathInsert),
-          i = 0,
-          fields = [];
           
-          for(i; i < this.modelProperties.length; i++){
-            fields.push(this.formatFactoryField(this.modelProperties[i]));
-          }
-
-          return newFactory
-            .replace("<fields>", fields.join(",\n"))
-            .replace("<modelName>",this.name);
-    },
-
-    formatFactoryField: function(modelField)
-    {
-          return '\t"'+ modelField[0] +'" => ' 
-                      + this.getFaker(modelField[1]);
-    },
-
-    formatMigrationField: function(modelField)
-    {
-          return '\t'+ this.getMigration(modelField[1])
-                     + "');"
-    },
-
-    getMigration: function (fieldType)
-    {
-      switch (fieldType) {
-          case 'string':
-            return "$table->string('";
-          case 'double':  
-            return "$table->double('";
-          case 'integer':
-            return "$table->integer('";
-          case 'datetime':
-            return "$table->datetime('";
-          case 'text':
-            return "$table->text('";
-          default:
-            return "$table->string('";
-      }
-    },
-
-      
-    getFaker: function(fieldType){
-       switch(fieldType){
-          case 'string':
-            return "$faker->name";
-          case 'double':  
-          case 'integer':
-            return "$faker->randomNumber(1)";
-          case 'datetime':
-            return "$faker->datetime()";
-          case 'text':
-            return "$faker->sentence";
-          default:
-            return '$faker->name';
-       }
-    },
-
-    _propertiesPrompt: function (as){
-        var done = as || this.async();
-
-        this.prompt({
-                type: 'input',
-                name: 'var_name',
-                message: 'Variable name'
-              },  function (answers) {
-                    this.modelProperties = answers.var_name;   
-                    //console.log(this.modelProperties);                 
-                     done();
-                    }.bind(this));
-    },
-
-   _typesPrompt: function (promise, modelFields) {
-
-                this.prompt([
-                {
-                type: 'rawlist',
-                name: 'var_type',
-                message: 'var type',
-                choices: [
-                  "string",
-                  "integer",
-                  "datetime",
-                  "text",
-                  "time",
-                  "blob",
-                  "double"
-                ]
-              }], function (answers) {
-                this.log('chosen');
-                
-                this._propertiesPrompt(promise);
-              }.bind(this));
-
-
-   },
-                 _combine: function(vars, types){
-                var combined = [];
-                vars.forEach(function(element) {
-                    combined.push(
-                        { 
-                        name: element,
-                        type: types[element+"_type"]
-                        }      
-                    );
-                }, this);
-                
-                return combined;
-          },
-            modelQuestions: function() {
-              if(this.options.fields){
-                var i = 0,
-                    fields = this.options.fields.split(','),
-                    typeQuestions = [],
-                    typeAnswers = [],
-                    done= this.async();
-                    
-                    for(i = 0; i < fields.length; i++)
-                    {
-                        typeQuestions.push({
-                            type: 'rawlist',
-                            name: fields[i]+'_type',
-                            message: 'field type for '+fields[i],
-                            choices: [
-                                "string",
-                                "integer",
-                                "datetime",
-                                "text",
-                                "time",
-                                "blob",
-                                "double"
-                            ]});
-                    }
-                    
-                    this.prompt(typeQuestions, function (answers) {
-                        this.modelProperties = this._combine(fields, answers);
-                        done();
-                }.bind(this));      
-              }
-          }
-  });
+          this.prompt(typeQuestions, function (answers) {
+              this.modelProperties = this._combine(fields, answers);
+              done();
+      }.bind(this));      
+    }
+  }
+});
 
