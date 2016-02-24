7df776729e4ca5aecd4932d7ef6667e4f2604a42
Tommy <tom.staszyszyn@gmail.com>
2016-02-24 21:52:03 +0000

    small refactor on main generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -48,33 +48,24 @@
     installing: function(){
       var success;
 
-
       if(this.policies.canCloneLaravel()) {
         this.log('Getting Laravel...');
-        success = commands.getLaravel() ? true : false;
-        if(success)
-        {
+        if(commands.getLaravel())
           this.config.set('laravel_cloned', true);
-        }
       }
 
       if(this.policies.canInstallComposer()) {
-          this.log('Getting Composer...');
-          success = commands.getComposer() ? true : false;
-
-          if(success)
+          if(commands.getComposer())
           {
             this.config.set('composer_installed', true);
             this.log('Running Composer...');
             success = commands.runComposer() ? 'done running composer install' :  'Got error while running composer install!';
             this.log(success);
           }
-
       }
 
       if(this.policies.canRunNpmInstall()){
-        this.log('Running npm install...');
-                    this.config.set('npm_install_runned', true);
+        this.config.set('npm_install_runned', true);
         this.npmInstall();
       }
 
